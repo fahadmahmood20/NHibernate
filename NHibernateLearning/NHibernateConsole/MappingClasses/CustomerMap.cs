f00ed27e1f814f17732cf4b7f736cfcbc55a8770
Fahad Mahmood <fahadmahmood20@gmail.com>
2020-02-20 00:21:10 +0500

    - Running First Fluenet NHibernate in Console

---
+++ NHibernateLearning/NHibernateConsole/MappingClasses/CustomerMap.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Mapping;
+using NHibernateConsole.DomainClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateConsole.MappingClasses
+{
+    public class CustomerMap : ClassMap<Customer>
+    {
+        public CustomerMap()
+        {
+            Id(c => c.Id);
+            Map(c => c.FirstName).Not.Nullable();
+            Map(c => c.MiddleName);
+            Map(c => c.LastName).Not.Nullable();
+        }
+    }
+}
